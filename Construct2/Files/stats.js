ad8c1e43b52a500cf846086eb8b06ca929c5f1a4
DouglasBarbino <douglas-448@hotmail.com>
2017-06-06 13:47:45 -0300

    Estatisticas 7 / Melhorias 33
    Implementado o envio da pontuação do jogador para o REMAR e corrigido
    alguns bugs com relação à pausa para o jogador ler as perguntas

--- Construct2/Files/stats.js
+++ Construct2/Files/stats.js
@@ -134,3 +134,26 @@ function sendPlayDataFinalLevel(nroDanos,nivel){
     console.log(nroDanos);
     console.log(nivel);
 }
+
+function sendRankingData(pontos){
+    var info = {};
+    var path;
+    if(window.location.hostname == "localhost" ){   // for localhost tests
+        path = "/exported-resource/saveStats"
+    }else {                                 // for web version in production, electron and crosswalk versions
+        path = "http://remar.dc.ufscar.br/exported-resource/saveStats"
+    }
+    $.getJSON("remar.json", function(json) {
+        info.exportedResourceId = json.exportedResourceId;
+        info.score = pontos;
+        info.gameType = 'ranking';
+        $.ajax({
+            type: "POST",
+            url: path,
+            data: info,
+            success: function(data) {
+            }
+        })
+    });
+    console.log(pontos);
+}
