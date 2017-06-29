92c864c25ee41b2a353407d771401bee13f53af0
Luiz Valério Neto <luizvneto@gmail.com>
2017-06-29 17:40:40 -0300

    Deploy do Protótipo

---
+++ Prototipo/06-23-2017/Deploy/offline.js
@@ -0,0 +1,19 @@
+﻿{
+	"version": 1498765775,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/soccer_ball-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/81308687cheeringcrowdsilhouettes-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
