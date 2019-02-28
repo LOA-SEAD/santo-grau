bc53d87d8a04fa673c2c6139a1efa18d865e1f50
Valério <luizvneto@gmail.com>
2019-02-28 17:44:18 -0300

    Correções Stats 3'
    - Criação de stats.js secundário só para testes
    - Remoção completa das estatísticas do RU
    - Correção completa dos IDs dos níveis
    - Correção do tempo total de jogo.

--- Construct2/Files/stats.js
+++ Construct2/Files/stats.js
@@ -1,215 +1,110 @@
 function sendDataTechnology(palavra,correta,resposta,nroPalavra,tentativas,venceu){
 	var info = {};
-    var path;
-    if(window.location.hostname == "localhost" ){   // for localhost tests
-        path = "/exported-resource/saveStats"
-    }else {                                 // for web version in production, electron and crosswalk versions
-        path = "http://remar.dc.ufscar.br/exported-resource/saveStats"
-	}
-	$.getJSON("remar.json", function(json) {
-        info.exportedResourceId = json.exportedResourceId;
-        info.gameLevelId = 0;
-        info.word = palavra;
-        info.correctAnswer = correta;
-        info.answer = resposta;
-        info.challengeId = nroPalavra;
-        info.numberTries = tentativas;
-        info.win = venceu;
-        info.size = 3;
-        info.gameLevelName = 'Fase Tecnologia';
-        info.gameType = 'shuffleWord';
-        $.ajax({
-            type: "POST",
-            url: path,
-            data: info,
-            success: function(data) {
-            }
-        })
-	});
-	console.log(palavra);
-	console.log(correta);
-    console.log(resposta);
-	console.log(nroPalavra);
-	console.log(tentativas);
-    console.log(venceu);
+    var path = "http://alfa.remar.online/stats/salveValerio";
+    
+    info.gameLevelId = 1;
+    info.word = palavra;
+    info.correctAnswer = correta;
+    info.answer = resposta;
+    info.challengeId = nroPalavra;
+    info.numberTries = tentativas;
+    info.win = venceu;
+    info.size = 3;
+    info.gameLevelName = 'Fase Tecnologia';
+    info.gameType = 'shuffleWord';
+    
+    console.log("Enviando sendDataTechnology");
+	console.log(info);
 }
 
 function sendDataGallery(arrastos,seqInicial){
     var info = {};
-    var path;
-    if(window.location.hostname == "localhost" ){   // for localhost tests
-        path = "/exported-resource/saveStats"
-    }else {                                 // for web version in production, electron and crosswalk versions
-        path = "http://remar.dc.ufscar.br/exported-resource/saveStats"
-    }
-    $.getJSON("remar.json", function(json) {
-        info.exportedResourceId = json.exportedResourceId;
-        info.gameLevelId = 1;
-        info.numberDrag = arrastos;
-        info.initialSequence = seqInicial;
-        info.win = true;
-        info.size = 1;
-        info.challengeId = 0;
-        info.gameLevelName = 'Fase Galeria';
-        info.gameType = 'dragPictures';
-        $.ajax({
-            type: "POST",
-            url: path,
-            data: info,
-            success: function(data) {
-            }
-        })
-    });
-    console.log(arrastos);
-    console.log(seqInicial);
+    var path = "http://alfa.remar.online/stats/salveValerio";
+
+    info.gameLevelId = 2;
+    info.numberDrag = arrastos;
+    info.initialSequence = seqInicial;
+    info.win = true;
+    info.size = 1;
+    info.challengeId = 0;
+    info.gameLevelName = 'Fase Galeria';
+    info.gameType = 'dragPictures';
+
+    console.log("Enviando sendDataGallery");
+	console.log(info);
 }
 
 function sendDataFinalLevel(pergunta,correta,nroPergunta,respostas,escolhida,acertou,tamanho,fase,nomeFase){
     var info = {};
-    var path;
-    if(window.location.hostname == "localhost" ){   // for localhost tests
-        path = "/exported-resource/saveStats"
-    }else {                                 // for web version in production, electron and crosswalk versions
-        path = "http://remar.dc.ufscar.br/exported-resource/saveStats"
-    }
-    $.getJSON("remar.json", function(json) {
-        info.exportedResourceId = json.exportedResourceId;
-        info.gameLevelId = fase;
-        info.question = pergunta;
-        info.correctAnswer = correta;
-        info.challengeId = nroPergunta;
-        info.choices = respostas;
-        info.answer = escolhida;
-        info.win = acertou;
-        info.size = tamanho;
-        info.gameLevelName = nomeFase;
-        info.gameType = 'multipleChoice';
-        $.ajax({
-            type: "POST",
-            url: path,
-            data: info,
-            success: function(data) {
-            }
-        })
-    });
-    console.log(pergunta);
-    console.log(correta);
-    console.log(nroPergunta);
-    console.log(respostas);
-    console.log(escolhida);
-    console.log(acertou);
-    console.log(tamanho);
-    console.log(fase);
-    console.log(nomeFase);
+    var path = "http://alfa.remar.online/stats/salveValerio";
+
+    info.gameLevelId = fase;
+    info.question = pergunta;
+    info.correctAnswer = correta;
+    info.challengeId = nroPergunta;
+    info.choices = respostas;
+    info.answer = escolhida;
+    info.win = acertou;
+    info.size = tamanho;
+    info.gameLevelName = nomeFase;
+    info.gameType = 'multipleChoice';
+
+    console.log("Enviando sendDataFinalLevel");
+	console.log(info);
+
 }
 
 function sendPlayDataGallery(terminou,nroDanos){
     var info = {};
-    var path;
-    if(window.location.hostname == "localhost" ){   // for localhost tests
-        path = "/exported-resource/saveDamageStats"
-    }else {                                 // for web version in production, electron and crosswalk versions
-        path = "http://remar.dc.ufscar.br/exported-resource/saveDamageStats"
-    }
-    $.getJSON("remar.json", function(json) {
-        info.exportedResourceId = json.exportedResourceId;
-        info.finish = terminou;
-        info.numberDamages = nroDanos;
-        info.gameType = 'SantoGrauGallery';
-        $.ajax({
-            type: "POST",
-            url: path,
-            data: info,
-            success: function(data) {
-            }
-        })
-    });
-    console.log(terminou);
-    console.log(nroDanos);
+    var path = "http://alfa.remar.online/stats/salveValerio";
+
+    info.finish = terminou;
+    info.numberDamages = nroDanos;
+    info.gameType = 'SantoGrauGallery';
+
+    console.log("Enviando sendPlayDataGallery");
+	console.log(info);
 }
 
 function sendPlayDataFinalLevel(nroDanos,nivel){
     var info = {};
-    var path;
-    if(window.location.hostname == "localhost" ){   // for localhost tests
-        path = "/exported-resource/saveDamageStats"
-    }else {                                 // for web version in production, electron and crosswalk versions
-        path = "http://remar.dc.ufscar.br/exported-resource/saveDamageStats"
-    }
-    $.getJSON("remar.json", function(json) {
-        info.exportedResourceId = json.exportedResourceId;
-        info.numberDamages = nroDanos;
-        info.level = nivel;
-        info.gameType = 'SantoGrauFinalLevel';
-        $.ajax({
-            type: "POST",
-            url: path,
-            data: info,
-            success: function(data) {
-            }
-        })
-    });
-    console.log(nroDanos);
-    console.log(nivel);
+    var path = "http://alfa.remar.online/stats/salveValerio";
+
+    info.numberDamages = nroDanos;
+    info.level = nivel;
+    info.gameType = 'SantoGrauFinalLevel';
+
+    console.log("Enviando sendPlayDataFinalLevel");
+    console.log(info);
+
 }
 
 function sendRankingData(pontos){
     var info = {};
-    var path;
-    if(window.location.hostname == "localhost" ){   // for localhost tests
-        path = "/exported-resource/saveScore"
-    }else {                                 // for web version in production, electron and crosswalk versions
-        path = "http://remar.dc.ufscar.br/exported-resource/saveScore"
-    }
-    $.getJSON("remar.json", function(json) {
-        info.exportedResourceId = json.exportedResourceId;
-        info.score = pontos;
-        $.ajax({
-            type: "POST",
-            url: path,
-            data: info,
-            success: function(data) {
-            }
-        })
-    });
-    console.log(pontos);
+    var path = "http://alfa.remar.online/stats/salveValerio";
+
+    info.score = pontos;
+
+    console.log("Enviando sendRankingData");
+	console.log(info);
 }
 
 function sendPlaytimeData(tempo,tipo,idJogo,idNivel,idDesafio){
     var info = {};
-    var path;
-    if(window.location.hostname == "localhost" ){   // for localhost tests
-        path = "/exported-resource/saveTimeStats"
-    }else {                                 // for web version in production, electron and crosswalk versions
-        path = "http://remar.dc.ufscar.br/exported-resource/saveTimeStats"
-    }
-    $.getJSON("remar.json", function(json) {
-        info.exportedResourceId = json.exportedResourceId;
-        info.time = tempo;
-        info.type = tipo;
-        info.gameId = idJogo;
-        if (idNivel != null){
-            info.gameLevel = idNivel;
-        }
-        if (idDesafio != null){
-            info.challengeId = idDesafio;
-        }
-        info.gameType = 'ConclusionTime';
-        $.ajax({
-            type: "POST",
-            url: path,
-            data: info,
-            success: function(data) {
-            }
-        })
-    });
-    console.log(tempo);
-    console.log(tipo);
-    console.log(idJogo);
+    var path = "http://alfa.remar.online/stats/salveValerio";
+
+    info.time = tempo;
+    info.type = tipo;
+    info.gameId = idJogo;
     if (idNivel != null){
-        console.log(idNivel);
+        info.gameLevel = idNivel;
     }
     if (idDesafio != null){
-        console.log(idDesafio);
+        info.challengeId = idDesafio;
     }
+
+    info.gameType = 'ConclusionTime';
+
+	console.log("Enviando sendRankingData");    
+    console.log(info);
 }
